35cad551a6ea72ee257c088f5713513b32e0e4ac
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-07-18 15:32:24 +0900

    Model, Repository

---
+++ src/main/java/com/jongkuen/inventory_app/repository/StockRepository.java
@@ -0,0 +1,11 @@
+package com.jongkuen.inventory_app.repository;
+
+import com.jongkuen.inventory_app.model.Stock;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+import java.util.Optional;
+
+public interface StockRepository extends JpaRepository<Stock, Long>, QuerydslPredicateExecutor<Stock> {
+    Optional<Stock> findByProductIdAndWarehouseId(Long productId, Long warehouseId);
+}
