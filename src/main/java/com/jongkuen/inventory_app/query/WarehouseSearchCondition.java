3b86b3051b8343989e3e6973452c1da7a10c3079
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-07-21 10:38:52 +0900

    Query(Stock,Warehouse)

---
+++ src/main/java/com/jongkuen/inventory_app/query/WarehouseSearchCondition.java
@@ -0,0 +1,11 @@
+package com.jongkuen.inventory_app.query;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class WarehouseSearchCondition {
+    private String name;
+    private String location;
+}
